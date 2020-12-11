7052acd433499f1f1990b6ee33b7170d545c28de
Mateus V. Farias <mateus_vinicius15@outlook.com>
2020-12-10 20:18:06 -0400

    feat: add typescript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>
+        <h1>Hello, World!</h1>
+      </main>
+    </div>
+  )
+}
